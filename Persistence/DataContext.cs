87f35c3e01a3a1d50349d44621df66f7c8ca9d98
Luna Goodnight <luna.lee@nextfortune.tw>
2022-11-19 20:00:08 +0800

    [Add] connectionString

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence;
 
 public class DataContext : DbContext
 {
-    
+    private readonly DbContextOptions _options;
+
+    public DataContext(DbContextOptions options) : base(options)
+    {
+        _options = options;
+        
+    }
+
+    public DbSet<Activity> Activities { get; set; }
 }

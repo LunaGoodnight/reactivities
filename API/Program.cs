c754209d01e6d2ea73c3cc5a66af3725b89b00d6
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-14 12:32:57 +0800

    [Add] cors

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy",
+        policy => { policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000"); });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

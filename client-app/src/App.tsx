a82760c2e872eeca4eb375b12677e02a85f8a3ae
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-14 23:33:46 +0800

    [Add] package

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,46 +1,20 @@
-
-import * as React from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
 
   const [act, setAct] = React.useState(null);
-
   React.useEffect(() => {
-
-
-
       fetch('https://localhost:44339/api/Activities').then( (res) => res.json()).then((response) => {
-
         setAct(response);
       })
-
-
   },[])
 
-
   React.useEffect(() => {
-
     console.log({act})
   }, [act])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>dsfasdf</div>
   );
 }
 

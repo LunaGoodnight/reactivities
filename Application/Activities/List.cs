777387efe7f4826991eb2c6d84172764073d26b8
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-20 23:12:20 +0800

    [Add] List class

---
+++ Application/Activities/List.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using MediatR;
+
+namespace Application.Activities;
+
+public class List
+{
+    public class Query : IRequest<List<Activity>> { }
+
+    public class Handler : IRequestHandler<Query, List<Activity>>
+    {
+        public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

3dd070d1cea8c40941f28b5fda4ccc5253a14608
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-23 17:28:34 +0800

    [Add] Query Details

---
+++ Application/Activities/Details.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using MediatR;
+
+namespace Application.Activities;
+
+public class Details
+{
+    public class Query : IRequest<Activity>
+    {
+        public Guid Id { get; set; }
+        
+    }
+}

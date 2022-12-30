62587cc2018c478bfbe56e22f72ec3704b243ef0
Luna Goodnight <luna.lee@nextfortune.tw>
2022-12-31 04:32:27 +0800

    [Add] Edit

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using MediatR;
+
+namespace Application.Activities;
+
+public class Edit
+{
+    public class Command : IRequest
+    {
+        public Activity Activity { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Command>
+    {
+        public Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

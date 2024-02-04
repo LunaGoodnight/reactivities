563ae7ee52064f276b131ed4afdab56a147cd101
Luna Goodnight <luna.lee@nextfortune.tw>
2024-02-04 20:54:02 +0800

    add token service

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.NameIdentifier, user.Id),
+            new Claim(ClaimTypes.Email, user.Email),
+        };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("super secret key"));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+        
+        return String.Empty;
+    }
+}

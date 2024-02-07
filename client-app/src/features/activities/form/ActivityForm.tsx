4db9b6d81674aa3b4d80e32fbeafbaee1dc2803c
Luna Goodnight <luna.lee@nextfortune.tw>
2024-02-08 06:39:22 +0800

    update form

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { v4 as uuid } from "uuid";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { format } from "date-fns";
 
 const FormBlockWrapper = styled.div`
   //width: 100%;
@@ -9,6 +13,7 @@ const FormBlockWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+
   input {
     width: 100%;
     padding: 1rem;
@@ -16,34 +21,117 @@ const FormBlockWrapper = styled.div`
 `;
 
 const ResetButton = styled.button`
-
- background: lightgray;
-`
+  background: lightgray;
+`;
 
 const ButtonRow = styled.div`
   display: flex;
   gap: 8px;
-  button {
 
+  button {
+    padding: 1rem 0;
     flex: 0 0 calc(50% - 4px);
   }
-`
+`;
 
 export const ActivityForm = () => {
+  const [category, setCategory] = useState("");
+  const [city, setCity] = useState("");
+  const [date, setDate] = useState<Date | undefined>();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [venue, setVenue] = useState("");
+  const [isShowDatePicker, setIsShowDatePicker] = useState(false);
+  const handleSubmit = async () => {
+    const apiUrl = `${process.env.REACT_APP_API_URL}/Activities`;
+    const data = {
+      id: uuid(),
+      title,
+      date,
+      city,
+      category,
+      description,
+      venue,
+    };
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      if (response) {
+        const final = response.json();
+        console.log({ final });
+      }
+    } catch (e) {
+      console.log({ e });
+    }
+  };
+  const handleSetDate = (data: any) => {
+    if (data) {
+      setDate(data);
+    }
+
+    setIsShowDatePicker(false);
+  };
+
+  let footer = <p>Please pick a day.</p>;
+  if (date) {
+    footer = <p>You picked {format(date, "PP")}.</p>;
+  }
   return (
     <FormBlockWrapper>
-      <input type="text" placeholder="Title" />
-      <input type="text" placeholder="Description" />
-      <input type="text" placeholder="Category" />
-      <input type="text" placeholder="Date" />
-      <input type="text" placeholder="City" />
-      <input type="text" placeholder="Venue" />
-        <ButtonRow>
+      <input
+        type="text"
+        placeholder="Category"
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="City"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button
+        type="button"
+        onClick={() => setIsShowDatePicker((prev) => !prev)}
+      >
+        Select Date
+      </button>
+      {isShowDatePicker && (
+        <DayPicker
+          mode="single"
+          selected={date}
+          onSelect={handleSetDate}
+          footer={footer}
+        />
+      )}
 
-            <ResetButton type="button">Reset</ResetButton>
-            <button type="button">Submit</button>
-        </ButtonRow>
+      <input
+        type="text"
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Venue"
+        value={venue}
+        onChange={(e) => setVenue(e.target.value)}
+      />
+      <ButtonRow>
+        <ResetButton type="button">Reset</ResetButton>
+        <button type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </ButtonRow>
     </FormBlockWrapper>
   );
 };
-

9483ade8b2475ef5db4bc43c7700a883ba7d8265
Luna Goodnight <luna.lee@nextfortune.tw>
2024-02-09 07:26:27 +0800

    mediator

---
+++ API/Controllers/MangaController.cs
@@ -0,0 +1,6 @@
+﻿namespace API.Controllers;
+
+public class MangaController : BaseApiController
+{
+    
+}
---
+++ Application/Manga/List.cs
@@ -0,0 +1,24 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Manga;
+
+public class List
+{
+    public class Query : IRequest<List<Link>> { }
+    public class Handler : IRequestHandler<Query, List<Link>>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Link>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return await _context.MangaLinks.ToListAsync();
+        }
+    }
+}

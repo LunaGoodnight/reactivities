f54dc83fbe6516030c4340dfa16b5556418e4b72
Luna Goodnight <luna.lee@nextfortune.tw>
2024-02-18 23:55:35 +0800

    add item

--- API/Controllers/ItemsController.cs
+++ API/Controllers/ItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Items;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -6,8 +7,28 @@ namespace API.Controllers;
 public class ItemsController : BaseApiController
 {
     [HttpGet]
-    public async Task<IActionResult> GetActivities()
+    public async Task<IActionResult> GetItems()
     {
         return HandleResult(await Mediator.Send(new List.Query()));
+    }
+    
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateItem(Item item)
+    {
+        return HandleResult(await Mediator.Send(new Create.Command { Item = item }));
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditItem(Guid id,Item item)
+    {
+        item.Id = id;
+        return HandleResult(await Mediator.Send(new Edit.Command { Item = item }));
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteItem(Guid id)
+    {
+        return HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
     }
 }

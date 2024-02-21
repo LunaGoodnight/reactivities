e26836a50bc4cd4199ae1498b93a86cdc0a8767a
Luna Goodnight <luna.lee@nextfortune.tw>
2024-02-22 01:38:24 +0800

    update item curd

--- client-app/src/features/anything/AnythingDashboard.tsx
+++ client-app/src/features/anything/AnythingDashboard.tsx
@@ -1,26 +1,31 @@
+import { randomUUID } from "crypto";
 import { useState } from "react";
 import styled from "styled-components";
-import { useGetItemListQuery } from "../../services/item";
+import { useAddItemMutation, useGetItemListQuery } from "../../services/item";
 import { MessageBox, ModalBox, ModalFrame } from "../../style/modalStyle";
+import { MaterialInput } from "../manga/form/MangaForm";
 import { Item } from "./Item";
+import { v4 as uuid } from "uuid";
 
 const ButtonRow = styled.div`
-display: flex;
-  
-  justify-content: flex-end;
-`
-const Button = styled.button`
+  display: flex;
 
+  justify-content: flex-end;
+`;
+const Button = styled.button`
   color: #fff;
   background: #3eb8b8;
   border-radius: 8px;
   padding: 1rem;
-`
+`;
 
 const CloseButton = styled.div`
   color: #8a8a8a;
+  position: absolute;
 
-`
+  right: 1rem;
+  top: 1rem;
+`;
 
 const InnerFlexBox = styled.div`
   width: 80%;
@@ -32,24 +37,79 @@ const InnerFlexBox = styled.div`
   padding: 2rem 0;
   margin: 0 auto;
 `;
+export const Input = styled.input`
+  padding: 1rem;
+  border: 1px solid lightgray;
+`;
 
+const WrapForm = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 80%;
+
+  gap: 1rem;
+`;
+
+const FormTitle = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  padding-bottom: 3rem;
+`;
 export const AnythingDashboard = () => {
   const { data } = useGetItemListQuery();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [addItem, { isLoading }] = useAddItemMutation();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   return (
     <InnerFlexBox>
-
-        <ButtonRow>
-            <Button type='button' onClick={() => setIsModalOpen(true)}>Create</Button>
-        </ButtonRow>
+      <ButtonRow>
+        <Button type="button" onClick={() => setIsModalOpen(true)}>
+          Create
+        </Button>
+      </ButtonRow>
       <div>
         {isModalOpen && (
           <ModalFrame>
             <ModalBox>
-              <MessageBox>
-                  <CloseButton>x</CloseButton>
+              <CloseButton>x</CloseButton>
+              <FormTitle>Insert Anything</FormTitle>
+              {isLoading ? (
+                "loading"
+              ) : (
+                <WrapForm>
+                  <Input
+                    placeholder="name"
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                  <Input
+                    placeholder="description"
+                    type="text"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
 
-              </MessageBox>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      if (name !== "" && description !== "") {
+                        addItem({
+                          id: uuid(),
+                          name,
+                          description,
+                        }).then(() => {
+                          setIsModalOpen(false);
+                        });
+                      }
+                    }}
+                  >
+                    Submit
+                  </button>
+                </WrapForm>
+              )}
             </ModalBox>
           </ModalFrame>
         )}
--- client-app/src/features/anything/Item.tsx
+++ client-app/src/features/anything/Item.tsx
@@ -1,10 +1,95 @@
-import {ItemInterface} from "../../app/models/activity";
+import { useState } from "react";
+import styled from "styled-components";
+import { ItemInterface } from "../../app/models/activity";
+import { useEditItemMutation } from "../../services/item";
+import { Input } from "./AnythingDashboard";
 
+const WrapRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  border-bottom: 1px solid #c1c1c1;
+  padding: 2rem 0;
+`;
+const Name = styled.div`
+  font-weight: bold;
+  font-size: 1.9rem;
+  color: #535353;
+  padding-bottom: 1rem;
+`;
+
+const EditForm = styled.div`
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+`;
+const Button = styled.button`
+  border-radius: 6px;
+  background: #dcdcdc;
+  cursor: pointer;
+  padding: 1rem;
+`;
+
+const SubmitButton = styled(Button)`
+  background: pink;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  gap: 1rem;
+`
 export const Item = ({ id, name, description }: ItemInterface) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [newName, setNewName] = useState(name);
+  const [newDescription, setNewDescription] = useState(description);
+
+  const [editItem, { isLoading, isSuccess }] = useEditItemMutation();
   return (
-    <div key={id}>
-      <div>{name}</div>
-      <div>{description}</div>
-    </div>
+    <WrapRow key={id}>
+      {isEdit ? (
+        <EditForm>
+          <Input value={newName} onChange={(e) => setNewName(e.target.value)} />
+          <Input
+            value={newDescription}
+            onChange={(e) => setNewDescription(e.target.value)}
+          />
+        </EditForm>
+      ) : (
+        <div>
+          {isLoading ? (
+            <div>loading</div>
+          ) : (
+            <>
+              <Name>{name}</Name>
+              <div>{description}</div>
+            </>
+          )}
+        </div>
+      )}
+
+      <div>
+        {isEdit ? (
+          <ButtonRow>
+            <Button type="button" onClick={() => setIsEdit(false)}>
+              Cancel
+            </Button>
+            <SubmitButton
+              type="button"
+              onClick={() => {
+                editItem({ id, name: newName, description: newDescription }).then(() => {
+                    setIsEdit(false)
+                });
+              }}
+            >
+              Submit
+            </SubmitButton>
+          </ButtonRow>
+        ) : (
+          <Button type="button" onClick={() => setIsEdit(true)}>
+            Edit
+          </Button>
+        )}
+      </div>
+    </WrapRow>
   );
 };
